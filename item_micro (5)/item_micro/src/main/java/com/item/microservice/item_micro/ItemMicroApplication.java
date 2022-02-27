d43a6a0658bfca56354837e0fef47c3c73cc0c8a
ayanaraj <100473207+ayanaraj@users.noreply.github.com>
2022-02-27 11:57:43 +0530

    Add files via upload

---
+++ item_micro (5)/item_micro/src/main/java/com/item/microservice/item_micro/ItemMicroApplication.java
@@ -0,0 +1,13 @@
+package com.item.microservice.item_micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemMicroApplication.class, args);
+	}
+
+}
